a02736e8d241f14f38ada42af8e73cee0f1eaa0d
Ran Tu <47575969+turanjade@users.noreply.github.com>
2022-04-23 23:03:08 +0800

    Update streamlit_homepage.py

--- streamlit_homepage.py
+++ streamlit_homepage.py
@@ -25,3 +25,10 @@ if st.sidebar.button('Working experience'):
   st.subheader('Working experience')
   st.write('2021-current, Associate Professor, School of Transportation, Southeast University')
   st.write('2020, Postdoctoral fellow, Civil Engineering, University of Toronto')
+
+if st.sidebar.button('Current projects'):
+  st.subheader('Current projects')
+  st.write('1.	Eco-driving Guidance Decision Modelling Based on Drivers' Dynamic Cognitive Behaviour, National Natural Science Foundation of China (Young Scholar), PI, 2022-2024'）
+  st.write('2.	Eco-driving Guidance Based on the Heterogeneity of Drivers’ Cognitive Workload, Natural Science Foundation of Jiangsu Province (Young Scholar), PI, 2021-2024')
+  st.write('3.	Modify Drivers' behaviour to Adapt for Lower Emissions, National Key R&D Program of China, Co-PI, 2021-2024')
+  st.write('4.	Dynamic Optimization of Electric Bus Services with Energy Consumption Uncertainties, FAW-Volkswagen China Environmental Protection Foundation Automotive Environmental Innovation Leading Program, PI, 2022-2023')          
